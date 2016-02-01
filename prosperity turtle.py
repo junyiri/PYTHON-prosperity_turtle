f7d7294136151490f6e1db78cdedfd0653e97d43
junyiri <junyiri@gmail.com>
2016-02-01 21:00:08 +0800

    Prosperity Turtle v2.0
    Done!

--- prosperity turtle.py
+++ prosperity turtle.py
@@ -25,6 +25,15 @@ def largeline(a,b,c,d):
     jimmy.seth(c)
     jimmy.forward(d)
 
+def line(a,b):
+    jimmy.width(3)
+    jimmy.pencolor("#F9D139")
+    jimmy.up()
+    jimmy.setpos(a,b)
+    jimmy.down()
+    jimmy.seth(0)
+    jimmy.forward(520)
+
 def xin():
     smallline(200,225,315,10) #dian
     smallline(185,210,0,40) #heng
@@ -65,9 +74,10 @@ def le():
     smallline(202,-167,270,28) #shu
     smallline(202,-195,0,50) #heng
     smallline(230,-175,270,65) #shu
-    smallline(230,-235,145,10) #gou
+    smallline(230,-240,145,10) #gou
     smallline(215,-210,225,20) #dian
     smallline(240,-210,315,20) #dian
+
 
 def wan():
     smallline(-250,220,0,60) #heng
@@ -76,9 +86,59 @@ def wan():
     smallline(-200,200,270,45) #zhe
     smallline(-200,155,135,10) #gou
 
+def shi():
+    smallline(-250,105,0,50) #heng
+    smallline(-245,95,270,15) #shu
+    smallline(-245,95,0,40) #heng
+    smallline(-205,95,270,15) #zhe
+    smallline(-245,80,0,40) #heng
+    smallline(-245,70,0,40) #heng
+    smallline(-205,70,270,20) #zhe
+    smallline(-250,60,0,50) #heng
+    smallline(-245,50,0,40) #heng
+    smallline(-225,110,270,80) #shu
+    smallline(-225,30,135,10) #gou
+
+def ru():
+    smallline(-235,-30,247.5,55) #pie
+    smallline(-255,-82,315,25) #zhe
+    smallline(-225,-50,247.5,55) #pie
+    smallline(-255,-62,0,25) #heng
+    jimmy.up()
+    jimmy.setpos(-220,-70)
+    jimmy.seth(0)
+    jimmy.down()
+    for x in range(0,4):
+        jimmy.forward(20)
+        jimmy.right(90) #kou
+
+def yi():
+    smallline(-233,-145,315,8) #dian
+    smallline(-260,-155,0,60) #heng
+    smallline(-243,-160,315,8) #dian
+    smallline(-217,-160,225,8) #dian
+    smallline(-260,-170,0,60) #heng
+    jimmy.up()
+    jimmy.setpos(-247.5,-177.5)
+    jimmy.seth(0)
+    jimmy.down()
+    for x in range(0,2):
+        jimmy.forward(35)
+        jimmy.right(90)
+        jimmy.forward(20)
+        jimmy.right(90) #kou
+    smallline(-245,-187.5,0,30) #heng
+    smallline(-255,-210,225,8) #dian
+    smallline(-205,-210,315,8) #dian
+    smallline(-247.5,-210,270,15) #shu
+    smallline(-247.5,-225,0,35) #zhe
+    smallline(-212.5,-225,90,5) #gou
+    smallline(-232,-207,315,8)
+
+
 def square():
     jimmy.width(2)
-    jimmy.speed(10)
+    jimmy.speed(12)
     jimmy.color("red","red")
     jimmy.up()
     jimmy.setpos(0,150)
@@ -102,24 +162,26 @@ def sqwd():
     jimmy.down()
     jimmy.circle(40) #kou
     largeline(-38,-45,0,80)
+
+def border():
+    line(-260,250)
+    line(-260,-270)
+
     
-
-
-
-
 xin()
 nian()
 kuai()
 le()
 
 wan()
+shi()
+ru()
+yi()
 
 square()
 sqwd()
 
-'''jimmy.up()
-jimmy.setpos(200,-150)
-jimmy.down()
-jimmy.pencolor("red")
-jimmy.stamp()'''
+border()
+
+
 
